54b2a54dc2ee4ca8edb3336a08b1751c46c4cc67
Minhyuk Sung <smh0816@gmail.com>
2017-01-10 17:15:19 -0800

    Updated remove small components module.

--- modules/remove_small_components.h
+++ modules/remove_small_components.h
@@ -7,9 +7,11 @@
 #define IGL_REMOVE_SMALL_COMPONENTS_H
 
 #include <igl/igl_inline.h>
-#include <igl/doublearea.h>
 #include <igl/facet_components.h>
+#include <igl/remove_unreferenced.h>
+#include <igl/slice.h>
 #include <igl/unique.h>
+#include <utils/utils.h>
 
 #include <map>
 #include <vector>
@@ -23,8 +25,9 @@ namespace igl
   IGL_INLINE void remove_small_components(
     const Eigen::PlainObjectBase<DerivedV>& V,
     const Eigen::PlainObjectBase<DerivedF>& F,
+    Eigen::PlainObjectBase<DerivedV>& newV,
     Eigen::PlainObjectBase<DerivedF>& newF,
-    const float eps = 1.0E-3f);
+    const float min_bbox_diag_ratio = 0.02f);
 
   template <
     typename MatV,
@@ -32,7 +35,7 @@ namespace igl
   IGL_INLINE void remove_small_components(
     MatV& V,
     MatF& F,
-    const float eps = 1.0E-3f);
+    const float min_bbox_diag_ratio = 0.02f);
 }
 
 //#ifndef IGL_STATIC_LIBRARY
@@ -46,16 +49,19 @@ template <
 IGL_INLINE void igl::remove_small_components(
     const Eigen::PlainObjectBase<DerivedV>& V,
     const Eigen::PlainObjectBase<DerivedF>& F,
+    Eigen::PlainObjectBase<DerivedV>& newV,
     Eigen::PlainObjectBase<DerivedF>& newF,
-    const float eps)
+    const float min_bbox_diag_ratio)
 {
+  using namespace Eigen;
   typedef typename DerivedV::Scalar ScalarV;
+  typedef typename DerivedF::Scalar ScalarF;
 
   // Find components.
-  Eigen::VectorXi C;
+  VectorXi C;
   igl::facet_components(F, C);
 
-  Eigen::VectorXi C_set;
+  VectorXi C_set;
   igl::unique(C, C_set);
   std::map<int, int> C_to_idx;
   const int num_C_set = C_set.size();
@@ -63,25 +69,31 @@ IGL_INLINE void igl::remove_small_components(
     C_to_idx[ C_set[cid] ] = cid;
   }
 
-  // Compute face areas.
-  Eigen::Matrix<ScalarV, Eigen::Dynamic, 1> DblFA;
-  igl::doublearea(V, F, DblFA);
-
-  // Compute component areas.
-  Eigen::Matrix<ScalarV, Eigen::Dynamic, 1> DblCA(num_C_set);
-  DblCA.setZero();
-  const int nF = F.rows();
-  for (int fid = 0; fid < nF; ++fid) {
-    const int cid = C_to_idx[ C[fid] ];
-    DblCA[cid] += DblFA[fid];
+  // Compute component bbox diagonals.
+  Matrix<ScalarV, Dynamic, 1> CBD(num_C_set);
+  for (int cid = 0; cid < num_C_set; ++cid) {
+    // FIXME(mhsung): Consider not to use utils functions.
+    const Matrix<ScalarF, Dynamic, 1> C_fids = Utils::find(C, C_set[cid]);
+    PlainObjectBase<DerivedF> CF;
+    igl::slice(F, C_fids, 1, CF);
+    const Map<Matrix<ScalarF, Dynamic, 1> > CF_vec(CF.data(), CF.size());
+    Matrix<ScalarF, Dynamic, 1> C_vids;
+    igl::unique(CF_vec, C_vids);
+    PlainObjectBase<DerivedV> CV;
+    igl::slice(V, C_vids, 1, CV);
+    const auto C_bb_min = CV.colwise().minCoeff();
+    const auto C_bb_max = CV.colwise().maxCoeff();
+    CBD[cid] = (C_bb_max - C_bb_min).norm();
   }
 
   // Remove faces in small components.
+  const ScalarV min_bbox_diag = min_bbox_diag_ratio * CBD.maxCoeff();
   std::vector<bool> is_component_small(num_C_set, false);
   for (int cid = 0; cid < num_C_set; ++cid) {
-    if (DblCA[cid] < 2 * eps) is_component_small[cid] = true;
+    if (CBD[cid] < min_bbox_diag) is_component_small[cid] = true;
   }
 
+  const int nF = F.rows();
   std::vector<int> new_to_old_fids;
   new_to_old_fids.reserve(nF);
   for (int fid = 0; fid < nF; ++fid) {
@@ -99,6 +111,10 @@ IGL_INLINE void igl::remove_small_components(
   for (int fid = 0; fid < new_nF; ++fid) {
     newF.row(fid) = F.row(new_to_old_fids[fid]);
   }
+
+  const Eigen::PlainObjectBase<DerivedF> newF_copy = newF;
+  VectorXi IX;
+  igl::remove_unreferenced(V, newF_copy, newV, newF, IX);
 }
 
 template <
@@ -107,10 +123,11 @@ template <
 IGL_INLINE void igl::remove_small_components(
     MatV& V,
     MatF& F,
-    const float eps)
+    const float min_bbox_diag_ratio)
 {
+  const MatV V_copy = V;
   const MatF F_copy = F;
-  remove_small_components(V,F_copy,F,eps);
+  remove_small_components(V_copy,F_copy,V,F,min_bbox_diag_ratio);
 }
 
 #endif
